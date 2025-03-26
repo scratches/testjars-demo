4c51c2d96eab8400d9d90f72b65262b6b18698c5
Dave Syer <dsyer@vmware.com>
2025-03-26 15:54:28 +0000

    Server is a resource server, client uses client_credentials

--- client/src/test/java/com/example/http/verification/client/ClientApplicationTests.java
+++ client/src/test/java/com/example/http/verification/client/ClientApplicationTests.java
@@ -1,19 +1,26 @@
 package com.example.http.verification.client;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.io.File;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.EnabledIf;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.experimental.boot.server.exec.CommonsExecWebServer;
 import org.springframework.experimental.boot.server.exec.CommonsExecWebServerFactoryBean;
 import org.springframework.experimental.boot.server.exec.FileClasspathEntry;
-import org.springframework.experimental.boot.test.context.DynamicProperty;
-import org.springframework.experimental.boot.test.context.EnableDynamicProperty;
+import org.springframework.experimental.boot.server.exec.MavenClasspathEntry;
+import org.springframework.experimental.boot.test.context.DynamicPortUrl;
+import org.springframework.experimental.boot.test.context.OAuth2ClientProviderIssuerUri;
 
-@SpringBootTest(properties = "remote.server.url=http://localhost:${remote.server.port}")
+import com.example.http.verification.client.clients.PersonService;
+
+@SpringBootTest
 @EnabledIf("isServerJarPresent")
 class ClientApplicationTests {
 
@@ -28,18 +35,29 @@ class ClientApplicationTests {
 	}
 
 	@Test
-	void contextLoads() {
+	void personServiceTest(@Autowired PersonService people) {
+		assertThat(people.test()).isEqualTo("test");
 	}
 
 	@TestConfiguration(proxyBeanMethods = false)
-	@EnableDynamicProperty
 	static class ExtraConfiguration {
 
 		@Bean
-		@DynamicProperty(name = "remote.server.port", value = "port")
-		static CommonsExecWebServerFactoryBean server() throws Exception {
+		@OAuth2ClientProviderIssuerUri
+		static CommonsExecWebServerFactoryBean authServer() {
 			return CommonsExecWebServerFactoryBean.builder()
-					.useRandomPort(false)
+					.defaultSpringBootApplicationMain()
+					.classpath(classpath -> classpath
+							.entries(MavenClasspathEntry.springBootStarter("oauth2-authorization-server")));
+		}
+
+		@Bean
+		@DynamicPortUrl(name = "remote.server.url")
+		static CommonsExecWebServerFactoryBean server(CommonsExecWebServer authServer) throws Exception {
+			String issuerUriProp = "spring.security.oauth2.client.provider.spring.issuer-uri";
+			String issuerUri = "http://127.0.0.1:" + authServer.getPort();
+			return CommonsExecWebServerFactoryBean.builder()
+					.systemProperties(s -> s.put(issuerUriProp, issuerUri))
 					.classpath(classpath -> classpath
 							.entries(new FileClasspathEntry(SERVER_JAR)));
 		}
